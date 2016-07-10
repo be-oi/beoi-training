c67a09830fe4bb0adc487bbecefb6f42b4ca0eab
Nico Ekkart <nicoekkart@gmail.com>
2016-07-10 15:22:28 +0200

    Adapted kruskal implementation to use UF of Robin. Mentioned Floyd-Warshal for minimax.

--- 16-minimum-spanning-tree/code/kruskal.cpp
+++ 16-minimum-spanning-tree/code/kruskal.cpp
@@ -4,35 +4,61 @@
 
 using namespace std;
 
+
+class UF {
+    private:
+        std::vector<int> parent;
+        std::vector<int> rank;
+
+    public:
+        UF(int num_elements) {
+            rank.assign(num_elements, 0);
+            parent.assign(num_elements, 0);
+            for (int i = 0; i < num_elements; i++)
+                parent[i] = i;      //All separate sets, each is its own root
+        }
+
+        int find(int a) { // Or getParent of course
+            if (parent[a] == a)
+                return a;
+            return parent[a] = find(parent[a]); //path compression
+        }
+
+        bool sameSet(int a, int b) {
+            return find(a) == find(b);
+        }
+
+        // Unfortunately, union is a keyword
+        void merge(int a, int b) {
+            int par_a = find(a);
+            int par_b = find(b);
+            if (par_a == par_b) return;
+
+            //heuristic by rank
+            if (rank[par_a] > rank[par_b]) {
+                parent[par_b] = par_a;
+            } else {
+                parent[par_a] = par_b;
+                if (rank[par_a] == rank[par_b])
+                    rank[par_b]++;
+            }
+        }
+};
+
+
 int n,m,total=0;
-int parent[1001],ran[1001];
 vector< pair<int, pair<int, int> > > G, M;
 
-int find(int x,int *parent) {
-    if(parent[x]!=x)
-        parent[x]=find(parent[x],parent);
-    return parent[x];
-}
-
-void uni(int x,int y) {
-    if(ran[y]<ran[x])
-        parent[y]=x;
-    else
-        parent[x]=y;
-    if(ran[x]==ran[y])
-        ran[y]++;
-}
-
 void Kruskal() {
-    int x,y;
+	UF uf(n);
     sort(G.begin(), G.end());
     for(int i=0;i<m;i++) {
-        x = find(G[i].second.first,parent);
-        y = find(G[i].second.second,parent);
-        if(x!=y) {
+    	int x = G[i].second.first;
+    	int y = G[i].second.second;
+        if(!uf.sameSet(x, y)) {
             M.push_back(G[i]);
             total += G[i].first;
-            uni(x,y);
+            uf.merge(x, y);
         }
     }
 }
@@ -40,10 +66,6 @@ void Kruskal() {
 int main()
 {
     cin>>n>>m;
-    for(int i = 0; i < n + 1; i++) {
-        parent[i]=i;
-        ran[i]=-1;
-    }
     for(int i  = 0; i < m; i++) {
         int u,v,w;
         cin>>u>>v>>w;
