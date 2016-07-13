8c531f87ca6765d3bda73249e91b8669e2e38982
Mattéo Couplet <matteo.couplet@gmail.com>
2016-07-13 16:11:35 +0200

    Update pdf

--- 08-segment-tree/listings/update.cpp
+++ 08-segment-tree/listings/update.cpp
@@ -5,7 +5,7 @@ void update(int p, int L, int R, int i) {
     // if i is in segment
     if(i >= L && i <= R) {
         // if new value is smaller, update
-        if(A[i] <= st[p])
+        if(A[i] <= A[st[p]])
             st[p] = i;
         update(2*p, L, (L+R)/2, i);
         update(2*p+1, (L+R)/2+1, R, i);
